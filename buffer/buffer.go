f8c299f8cfe874b0a40d91ef722d5d9cc446f306
yamatcha <sojusojuonetmrlarcbz@gmail.com>
2019-08-27 20:13:58 +0900

    debug global time program

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -19,10 +19,11 @@ type Buffer struct {
 
 type Buffers map[string]Buffer
 
-type Result_data struct {
+type ResultData struct {
 	MaxPacketNum    int
 	AccessCount     int
 	CurrentSecCount int
+	CurrentTimeCount int
 	BufMax          int
 	PacketNumAll    int
 	AccessPers      []int
@@ -33,10 +34,10 @@ func GetDuration(first time.Time, now time.Time) float64 {
 	return now.Sub(first).Seconds()
 }
 
-func (buf Buffers) CheckBufferTime(bufList []string, currentTime time.Time, time_width float64, result Result_data) (Buffers, []string, Result_data) {
+func (buf Buffers) CheckBufferTime(bufList []string, currentTime time.Time, timeWidth float64, result ResultData) (Buffers, []string, ResultData) {
 	i := 0
 	for _, k := range bufList {
-		if GetDuration(buf[k].FirstTime, currentTime) > time_width || result.EndFlag == true {
+		if GetDuration(buf[k].FirstTime, currentTime) > timeWidth || result.EndFlag == true {
 			result.AccessCount++
 			result.AccessPers[result.CurrentSecCount]++
 			if result.MaxPacketNum < len(*(buf[k].TimeList)) {
@@ -52,8 +53,8 @@ func (buf Buffers) CheckBufferTime(bufList []string, currentTime time.Time, time
 	return buf, nil, result
 }
 
-func CheckCurrentSec(std_time time.Time, currentTime time.Time, time_width float64, result Result_data) Result_data {
-	if GetDuration(std_time, currentTime) > float64(result.CurrentSecCount+1)*time_width {
+func CheckCurrentSec(std_time time.Time, currentTime time.Time, perSec float64, result ResultData) ResultData {
+	if GetDuration(std_time, currentTime) > float64(result.CurrentSecCount+1)*perSec {
 		result.AccessPers = append(result.AccessPers, 0)
 		result.CurrentSecCount++
 		return result
@@ -61,7 +62,7 @@ func CheckCurrentSec(std_time time.Time, currentTime time.Time, time_width float
 	return result
 }
 
-func (buf Buffers) AppendBuffer(bufList []string, currentTime time.Time, fivetuple string, result Result_data) (Buffers, []string, Result_data) {
+func (buf Buffers) AppendBuffer(bufList []string, currentTime time.Time, fivetuple string, result ResultData) (Buffers, []string, ResultData) {
 	_, ok := buf[fivetuple]
 	if !ok {
 		new_timelist := []float64{0.0}
@@ -81,14 +82,32 @@ func (buf Buffers) AppendBuffer(bufList []string, currentTime time.Time, fivetup
 
 //using in Global time base func
 
-func (buf Buffers) CheckGlobalTime(bufList []string, std_time time.Time, currentTime time.Time, time_width float64, result Result_data) (Buffers, []string, Result_data) {
-	if GetDuration(std_time, currentTime) > float64(result.CurrentSecCount+1)*time_width {
-		result.AccessPers = append(result.AccessPers, 0)
-		result.CurrentSecCount++
+func (buf Buffers) CheckGlobalTime(bufList []string, std_time time.Time, currentTime time.Time, timeWidth float64, perSec float64, result ResultData) (Buffers, []string, ResultData) {
+	duration := GetDuration(std_time, currentTime)
+	if  duration > float64(result.CurrentSecCount+1)*timeWidth {
+		result.AccessCount+=len(bufList)
+		result.AccessPers[result.CurrentSecCount]+=len(bufList)
 		bufList = []string{}
 		buf = Buffers{}
 	}
-	result.AccessCount++
-	result.AccessPers[result.CurrentSecCount]++
+	if duration > float64(result.CurrentTimeCount+1)*perSec{
+		result.AccessPers = append(result.AccessPers, 0)
+		result.CurrentTimeCount++
+	}
+	return buf, bufList, result
+}
+
+func (buf Buffers) CheckGlobalTimeIdeal(bufList []string, std_time time.Time, currentTime time.Time, timeWidth float64, perSec float64, result ResultData) (Buffers, []string, ResultData) {
+	duration := GetDuration(std_time, currentTime)
+	if  duration > float64(result.CurrentSecCount+1)*timeWidth {
+		result.AccessCount+=len(bufList)
+		result.AccessPers[result.CurrentSecCount]+=len(bufList)
+		bufList = []string{}
+		buf = Buffers{}
+	}
+	if duration > float64(result.CurrentTimeCount+1)*perSec{
+		result.AccessPers = append(result.AccessPers, 0)
+		result.CurrentTimeCount++
+	}
 	return buf, bufList, result
 }
--- main.go
+++ main.go
@@ -29,14 +29,13 @@ const (
 	maxSec  int     = 900
 )
 
-func packetTimeBase(buf buffer.Buffers, bufList []string, result buffer.Result_data) (buffer.Buffers, []string, buffer.Result_data) {
-	var std_time time.Time
+func packetTimeBase(buf buffer.Buffers, bufList []string, result buffer.ResultData) (buffer.Buffers, []string, buffer.ResultData) {
+	var stdTime time.Time
 	var currentTime time.Time
 
 	// read time width and buffer size
 	flag.Parse()
-	time_width, _ := strconv.ParseFloat(flag.Arg(0), 64)
-	// bufsize, _ := strconv.ParseFloat(flag.Arg(1), 64)
+	timeWidth, _ := strconv.ParseFloat(flag.Arg(0), 64)
 
 	// open csv
 	file, err := os.Open(csvpath)
@@ -60,26 +59,26 @@ func packetTimeBase(buf buffer.Buffers, bufList []string, result buffer.Result_d
 		fiveTuple := line[0]
 		currentTime, _ = time.Parse(time.RFC3339Nano, line[1])
 		if i == 0 {
-			std_time = currentTime
+			stdTime = currentTime
 		}
-		buf, bufList, result = buf.CheckBufferTime(bufList, currentTime, time_width, result)
+		buf, bufList, result = buf.CheckBufferTime(bufList, currentTime, timeWidth, result)
 		if result.CurrentSecCount != int(float64(maxSec)/perSec) {
-			result = buffer.CheckCurrentSec(std_time, currentTime, perSec, result)
+			result = buffer.CheckCurrentSec(stdTime, currentTime, perSec, result)
 		}
 		buf, bufList, result = buf.AppendBuffer(bufList, currentTime, fiveTuple,result)
 	}
 	result.EndFlag = true
-	buf, bufList, result = buf.CheckBufferTime(bufList, currentTime, time_width, result)
+	buf, bufList, result = buf.CheckBufferTime(bufList, currentTime, timeWidth, result)
 	return buf, bufList, result
 }
 
-func globalTimeBase(buf buffer.Buffers, bufList []string, result buffer.Result_data) (buffer.Buffers, []string, buffer.Result_data) {
-	var std_time time.Time
+func globalTimeBase(buf buffer.Buffers, bufList []string, result buffer.ResultData, ideal bool) (buffer.Buffers, []string, buffer.ResultData) {
+	var stdTime time.Time
 	var currentTime time.Time
 
 	// read time width and buffer size
 	flag.Parse()
-	time_width, _ := strconv.ParseFloat(flag.Arg(0), 64)
+	timeWidth, _ := strconv.ParseFloat(flag.Arg(0), 64)
 	// bufsize, _ := strconv.ParseFloat(flag.Arg(1), 64)
 
 	// open csv
@@ -104,31 +103,39 @@ func globalTimeBase(buf buffer.Buffers, bufList []string, result buffer.Result_d
 		fiveTuple := line[0]
 		currentTime, _ = time.Parse(time.RFC3339Nano, line[1])
 		if i == 0 {
-			std_time = currentTime
+			stdTime = currentTime
 		}
-		buf, bufList, result = buf.CheckGlobalTime(bufList, std_time, currentTime, time_width, result)
+		if ideal==false{
+			buf, bufList, result = buf.CheckGlobalTime(bufList, stdTime, currentTime, timeWidth, perSec, result)
+		}else {
+			buf, bufList, result = buf.CheckGlobalTimeIdeal(bufList, stdTime, currentTime, timeWidth, perSec, result)
+		}
 		buf, bufList, result = buf.AppendBuffer(bufList, currentTime, fiveTuple, result)
 	}
 	result.EndFlag = true
 	result.PacketNumAll = i
-	buf, bufList, result = buf.CheckGlobalTime(bufList, std_time, currentTime, time_width, result)
+	if ideal==false{
+		buf, bufList, result = buf.CheckGlobalTime(bufList, stdTime, currentTime, timeWidth, perSec, result)
+	}else {
+		buf, bufList, result = buf.CheckGlobalTimeIdeal(bufList, stdTime, currentTime, timeWidth, perSec, result)
+	}
 	return buf, bufList, result
 }
 
 func main() {
 	buf := buffer.Buffers{}
 	bufList := []string{}
-	result := buffer.Result_data{0, 0, 0, 0, 0, []int{0}, false}
+	result := buffer.ResultData{0, 0, 0, 0, 0, 0, []int{0}, false}
 
 	// buf, bufList, result = packetTimeBase(buf,bufList,result)
-	buf, bufList, result = globalTimeBase(buf, bufList, result)
+	buf, bufList, result = globalTimeBase(buf, bufList, result, false)
 
 	// print result
 
-	fmt.Println(result.BufMax)
-	// for i, v := range result.AccessPers {
-	// 	fmt.Println(float64(i+1)*(perSec), v)
-	// }
+	// fmt.Println(result.BufMax)
+	for i, v := range result.AccessPers {
+		fmt.Println(float64(i+1)*(perSec), v)
+	}
 	// fmt.Println(result.MaxPacketNum, result.AccessCount, float64(result.AccessCount)/float64(result.PacketNumAll),result.PacketNumAll)
 
 }
